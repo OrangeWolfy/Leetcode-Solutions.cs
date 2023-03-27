bcb7dcd827f568b66af40de7564b9f7154e3ef69
OrangeWolfy <orange007wolfy@gmail.com>
2023-03-26 22:41:44 -0600

    New solutions

--- 13. Roman to Integer.cs
+++ 13. Roman to Integer.cs
@@ -1,89 +1,28 @@
 ﻿using System;
 
-namespace Roman_to_Integer
+namespace Name
 {
     class Program
     {
         static void Main(string[] args)
         {
-            while (true)
+            Console.WriteLine(RomanToInteger("IV"));
+            Console.WriteLine(RomanToInteger("III"));
+        }
+        static int RomanToInteger(string s)
+        {
+            Dictionary<string, int> romanNumbersDictionary = new() { { "I", 1 }, { "IV", 4 }, { "V", 5 }, { "IX", 9 }, { "X", 10 }, { "XL", 40 }, { "L", 50 }, { "XC", 90 }, { "C", 100 }, { "CD", 400 }, { "D", 500 }, { "CM", 900 }, { "M", 1000 } };
+            int integer = 0;
+            for (int i = 0; i < s.Length; i++)
             {
-                Console.Write("Enter your roman to integer number: ");
-                string s = Console.ReadLine();
-                if (s == null)
+                if (i != s.Length - 1)
                 {
-                    s = "null";
+                    if (romanNumbersDictionary[s[i].ToString()] < romanNumbersDictionary[s[i + 1].ToString()]) integer += romanNumbersDictionary[s[i].ToString() + s[++i].ToString()];
+                    else integer += romanNumbersDictionary[s[i].ToString()];
                 }
-                int result = Roman_to_Integer(s);
-                Console.WriteLine("{0} = {1}", s, result);
+                else integer += romanNumbersDictionary[s[i].ToString()];
             }
-            static int Roman_to_Integer(string s)
-            {
-                char[] characters = s.ToCharArray();
-                int result = 0;
-                for (int i = 0; i < characters.Length; i++)
-                {
-                    if (characters[i] == 'I')
-                    {
-                        result += 1;
-                    }
-                    else if (characters[i] == 'V')
-                    {
-                        result += 5;
-                    }
-                    else if (characters[i] == 'X')
-                    {
-                        result += 10;
-                    }
-                    else if (characters[i] == 'L')
-                    {
-                        result += 50;
-                    }
-                    else if (characters[i] == 'C')
-                    {
-                        result += 100;
-                    }
-                    else if (characters[i] == 'D')
-                    {
-                        result += 500;
-                    }
-                    else if (characters[i] == 'M')
-                    {
-                        result += 1000;
-                    }
-                }
-                int _i;
-                for (int i = 0; i < characters.Length; i++)
-                {
-                    _i = i;
-                    _i++;
-                    if (_i < characters.Length && characters[i] == 'C' && characters[_i] == 'M')
-                    {
-                        result -= 200;
-                    }
-                    if (_i < characters.Length && characters[i] == 'X' && characters[_i] == 'L')
-                    {
-                        result -= 20;
-                    }
-                    if (_i < characters.Length && characters[i] == 'X' && characters[_i] == 'C')
-                    {
-                        result -= 20;
-                    }
-                    if (_i < characters.Length && characters[i] == 'C' && characters[_i] == 'D')
-                    {
-                        result -= 200;
-                    }
-                    if (_i < characters.Length && characters[i] == 'I' && characters[_i] == 'X')
-                    {
-                        result -= 2;
-                    }
-                    if (_i < characters.Length && characters[i] == 'I' && characters[_i] == 'V')
-                    {
-                        result -= 2;
-                    }
-                }
-                return result;
-            }
+            return integer;
         }
     }
 }
